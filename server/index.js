ee13336abc81e1c7f03b0d1fd87ffc7a1436c961
Melody <Melody@Melodys-MacBook.local>
2017-12-19 14:42:42 -0700

    step 4

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const checkForSession = require('./middlewares/checkForSession');
 require('dotenv').config();
 
 app.use(express());
@@ -10,6 +11,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
+
+app.use(checkForSession);
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`server listening on port ${port}`))
