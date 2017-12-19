a7bd527e27969e3a62cfbf8e0f6c319c509b2c9c
Melody <Melody@Melodys-MacBook.local>
2017-12-19 15:00:07 -0700

    step 5

--- server/index.js
+++ server/index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const checkForSession = require('./middlewares/checkForSession');
+const swag_controller = require('./controllers/swag_controller');
 require('dotenv').config();
 
-app.use(express());
+const app = express();
+
 app.use(bodyParser.json());
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
-}))
-
+}));
 app.use(checkForSession);
+
+app.get('/api/swag', swag_controller.read);
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`server listening on port ${port}`))
